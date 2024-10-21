7ba0bec67bf977a782e8574ea4c8ce41b2240235
phantomcancode <vodumeh@gmail.com>
2024-10-21 01:44:03 +0100

    resolving deployment errors

--- src/app/watch/(components)/VideoPlayer.jsx
+++ src/app/watch/(components)/VideoPlayer.jsx
@@ -1,38 +1,66 @@
 "use client";
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
+import "@vidstack/react/styles.css"; 
+import { MediaPlayer, MediaProvider, Poster, Track } from "@vidstack/react";
+import { PlayIcon } from "@vidstack/react/icons";
 import draw from "../../../asset/draw.svg";
 import playicon from "../../../../public/assets/playicon.svg";
 import Image from "next/image";
 import { useSelector } from "react-redux";
+// import '@vidstack/react/player/styles/default/theme.css';
+// import '@vidstack/react/player/styles/default/layouts/video.css';
 
 const VideoPlayer = ({ videoUrl, open, setopen }) => {
   const { lesson } = useSelector((state) => state.courses);
-  const [pausePlay, setpausePlay] = useState(false);
+  console.log("Video Link", lesson);
 
   const handleDownload = async () => {
     const url = `https://res.cloudinary.com/dmpqdaupc/${lesson.pdf_file}`;
+    // Fetch the file from the URL
+
     try {
+      // Fetch the file from the URL
       const response = await fetch(url);
+
       if (!response.ok) {
         throw new Error("Failed to fetch file");
       }
+
+      // Convert the response to a blob
       const blob = await response.blob();
+
+      // Create a temporary URL for the blob
       const blobUrl = window.URL.createObjectURL(blob);
+
+      // Create a temporary anchor element
       const link = document.createElement("a");
       link.href = blobUrl;
-      link.download = `${lesson?.title}.pdf`;
+      link.download = `${lesson?.title}.pdf`; // Set the default file name
+
+      // Append link to the document body
       document.body.appendChild(link);
+
+      // Programmatically click the link to trigger the download
       link.click();
+
+      // Remove the link from the document
       document.body.removeChild(link);
+
+      // Revoke the blob URL to free up memory
       window.URL.revokeObjectURL(blobUrl);
     } catch (error) {
       console.error("Download failed:", error);
     }
   };
 
+  const [pausePlay, setpausePlay] = useState(false);
   return (
-    <div className="col-span-12 md:col-span-9 h-screen gap-y-4 p-4 md:px-10 flex-1 overflow-y-auto w-full">
+    <div
+      className="col-span-12 md:col-span-9  h-screen gap-y-4 p-4 md:px-10 flex-1 overflow-y-auto w-full
+    "
+    >
+      {/* {videoUrl} */}
       <div>
         <Image
           src={draw}
@@ -43,8 +71,25 @@ const VideoPlayer = ({ videoUrl, open, setopen }) => {
           alt="Draw"
         />
 
+        {/* {!lesson.pdf_file&& (
+          <ReactPlayer
+            url={`https://res.cloudinary.com/dmpqdaupc/` +videoUrl}
+            width={"100%"}
+            height={"448px"}
+            controls
+            config={{
+              file: {
+                attributes: {
+                  controlsList: "nodownload", // Prevents download option
+                  disablePictureInPicture: true, // Disables PiP mode
+                },
+              },
+            }}
+          />
+        )} */}
+
         {!lesson.pdf_file && (
-          <div className="relative w-full cursor-pointer">
+          <div  className="relative w-full cursor-pointer">
             <ReactPlayer
               url={`https://res.cloudinary.com/dmpqdaupc/${videoUrl}`}
               width={"100%"}
@@ -56,8 +101,8 @@ const VideoPlayer = ({ videoUrl, open, setopen }) => {
               config={{
                 file: {
                   attributes: {
-                    controlsList: "nodownload",
-                    disablePictureInPicture: true,
+                    controlsList: "nodownload", // Prevents download option
+                    disablePictureInPicture: true, // Disables PiP mode
                   },
                 },
               }}
@@ -78,13 +123,15 @@ const VideoPlayer = ({ videoUrl, open, setopen }) => {
           </div>
         )}
         {!videoUrl && (
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center  items-center">
             <p className="">
-              Download the PDF below to access resources for this lesson
+              Donwload the PDF below to access resources for this lesson
             </p>
             <button
+              // href={`https://res.cloudinary.com/dmpqdaupc/${selector.pdflink}`}
+              // target="_blank"
               onClick={handleDownload}
-              className="bg-main px-4 py-2 rounded-lg text-white mt-4"
+              className="bg-main px-4 py-2 rounded-lg text-white mt-4 "
             >
               Download Pdf
             </button>
--- src/app/watch/page.jsx
+++ src/app/watch/page.jsx
@@ -1,14 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import TopBarNav from "./(components)/TopBarNav";
 import SideNav from "./(components)/SideNav";
 import "next-cloudinary/dist/cld-video-player.css";
-import { Video } from "./video/Video";
 import VideoPlayer from "./(components)/VideoPlayer";
 import Loading from "../dashboard/(dashboardcomponents)/loading";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
 import secureLocalStorage from "react-secure-storage";
+
 const Watch = () => {
   const [videoUrl, setVideoUrl] = useState("");
   const [open, setopen] = useState(false);
@@ -18,11 +18,10 @@ const Watch = () => {
 
   const params = useSearchParams();
   const token = secureLocalStorage.getItem("token");
-  console.log("tk", token);
-
   const id = params.get("id");
 
   useEffect(() => {
+    // Fetching course details
     axios
       .get(
         `https://edtech-backend-q2ud.onrender.com/course_details/api/courses/${id}`,
@@ -33,7 +32,6 @@ const Watch = () => {
         }
       )
       .then((res) => {
-        console.log("Video", res.data.data);
         setVideoData(res.data.data);
         setContentData(res.data.data);
       })
@@ -43,31 +41,32 @@ const Watch = () => {
       .finally(() => {
         setLoading(false);
       });
-  }, []);
+  }, [id, token]);
 
   if (loading) {
     return <Loading />;
   }
 
   return (
-    <div>
-      <div className="w-full">
-        <TopBarNav />
-        <div className="h-screen flex w-full ">
-          <SideNav
-            videoData={videoData}
-            courseContentData={courseContentData}
-            setVideoUrl={setVideoUrl}
-            setopen={setopen}
-            open={open}
-          />
+    <div className="w-full">
+      <TopBarNav />
+      <div className="h-screen flex w-full ">
+        <SideNav
+          videoData={videoData}
+          courseContentData={courseContentData}
+          setVideoUrl={setVideoUrl}
+          setopen={setopen}
+          open={open}
+        />
+        {/* Wrap VideoPlayer in Suspense */}
+        <Suspense fallback={<Loading />}>
           <VideoPlayer
             videoUrl={videoUrl}
             courseContentData={courseContentData}
             setopen={setopen}
             open={open}
           />
-        </div>
+        </Suspense>
       </div>
     </div>
   );
