7e89ae140e39623af0941aa7addd6a50f294620c
phantomcancode <vodumeh@gmail.com>
2025-06-04 18:04:26 +0100

    feat: download button for downloading resources for each lessons

--- src/app/dashboard/watch/(components)/SideNav.jsx
+++ src/app/dashboard/watch/(components)/SideNav.jsx
@@ -16,6 +16,7 @@ import SideNavTab from "./SideNavTab";
 
 import { Accordion, AccordionItem } from "@heroui/accordion";
 import { IoMdStopwatch } from "react-icons/io";
+import { RiDownload2Line } from "react-icons/ri";
 
 
 
@@ -99,19 +100,50 @@ const SideNav = ({
                           <div className=" text-base text-grayTwo font-normal min-h-full">
                             {
                               module.lessons.map((lesson, index) => (
-                                <div key={index} className={`p-1 cursor-pointer w-full flex justify-start items-start gap-2 ${videoUrl ? 'bg-[#d1d2df]' : ''}`}>
-                                  <div className='w-3 h-3 rounded-full border border-grayTwo mt-1'></div>
-                                  <div className="flex flex-col w-full justify-start items-start gap-2 h-[65px]">
-                                    <div onClick={() => setVideoUrl(lesson.link)} className="w-full flex justify-start items-center gap-2">
-                                      <p className="text-md font-medium">{index+1}.</p>
-                                      <p className="text-md font-medium">{lesson.name}</p>
-                                    </div>
+                                <div key={index} className='w-full flex flex-col justify-end items-end gap-3'>
+                                  <div className={`p-1 cursor-pointer w-full flex justify-start items-start gap-2 ${videoUrl ? 'bg-[#d1d2df]' : ''}`}>
+                                    <div className='w-3 h-3 rounded-full border border-grayTwo mt-1'></div>
+                                    <div className="flex flex-col w-full justify-start items-start gap-2 h-[65px]">
+                                      <div onClick={() => setVideoUrl(lesson.link)} className="w-full flex justify-start items-center gap-2">
+                                        <p className="text-md font-medium">{index+1}.</p>
+                                        <p className="text-md font-medium">{lesson.name}</p>
+                                      </div>
 
-                                    <div className="flex justify-start items-center gap-1">
-                                      <IoMdStopwatch size={22} />
-                                      <p className="text-sm font-normal">{lesson.duration} mins</p>
+                                      <div className="flex justify-start items-center gap-1">
+                                        <IoMdStopwatch size={22} />
+                                        <p className="text-sm font-normal">{lesson.duration} mins</p>
+                                      </div>
                                     </div>
+                                    {lesson.resources?.length > 0 && (
+                                    <div className="space-y-3">
+                                      {lesson.resources.map((resource) => {
+                                        const fileType = resource.link.split('.').pop().toUpperCase();
+                                        return (
+                                          <div className="p-3 flex justify-center items-center gap-2 text-main border-main bg-transparent rounded-[4px] font-medium text-md" key={resource.uid}>
+                                            <a
+                                              href={`${process.env.NEXT_PUBLIC_BASE_URL}storage/resources/${resource.link}`}
+                                              target="_blank"
+                                              rel="noopener noreferrer"
+                                            >
+                                              <RiDownload2Line/> Download ({fileType})
+                                            </a>
+                                          </div>
+                                        );
+                                      })}
+                                    </div>
+                                  )}
                                   </div>
+
+                                  {/* {
+                                    lesson.resources.map((resource) => (
+                                      <button onClick={} className='px-3 p flex justify-center items-center gap-2 text-main border-main bg-transparent rounded-[4px] font-medium text-md'>
+                                        
+                                        <p>Resources</p>
+                                      </button>
+                                    ))
+                                  } */}
+
+                                  
                                 </div>
                               ))
                             }
